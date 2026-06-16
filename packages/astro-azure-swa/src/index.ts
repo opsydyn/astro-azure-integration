6d3c2c7e5363e94782862b32a5199a613311339b
Alan P Currie <alanc909@outlook.com>
2026-06-16 15:23:59 +0100

    feat: register astro 6 azure swa adapter

--- packages/astro-azure-swa/src/index.ts
+++ packages/astro-azure-swa/src/index.ts
@@ -1,14 +1,45 @@
 import type { AstroIntegration } from "astro";
 
+import { generateAzureSwaFiles } from "./generate.js";
+
+const ADAPTER_NAME = "@opsydyn/astro-azure-swa";
+
 export interface AzureSwaAdapterOptions {
   functionName?: string;
 }
 
 export default function azureSwaAdapter(
-  _options: AzureSwaAdapterOptions = {},
+  options: AzureSwaAdapterOptions = {},
 ): AstroIntegration {
+  const functionName = options.functionName ?? "server";
+
   return {
-    name: "@opsydyn/astro-azure-swa",
-    hooks: {},
+    name: ADAPTER_NAME,
+    hooks: {
+      "astro:config:done": ({ setAdapter }) => {
+        setAdapter({
+          name: ADAPTER_NAME,
+          entrypointResolution: "auto",
+          serverEntrypoint: `${ADAPTER_NAME}/server`,
+          supportedAstroFeatures: {
+            staticOutput: "stable",
+            serverOutput: "stable",
+            hybridOutput: "stable",
+            sharpImageService: "stable",
+            envGetSecret: "stable",
+          },
+          adapterFeatures: {
+            buildOutput: "server",
+            middlewareMode: "classic",
+          },
+        });
+      },
+      "astro:build:done": async ({ dir }) => {
+        await generateAzureSwaFiles({
+          distDir: dir,
+          functionName,
+        });
+      },
+    },
   };
 }
